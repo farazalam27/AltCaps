e582e130affe12a27c19c0e5828f900389d338bf
farazalam27 <farazalam27@gmail.com>
2025-05-06 15:10:36 -0400

    Added the host container for the extension

---
+++ AlternatingCapsHost/AlternatingCapsHostApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlternatingCapsHostApp.swift
+//  AlternatingCapsHost
+//
+//  Created by Faraz Alam on 5/5/25.
+//
+
+import SwiftUI
+
+@main
+struct AlternatingCapsHostApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AlternatingCapsHostTests/AlternatingCapsHostTests.swift
@@ -0,0 +1,17 @@
+//
+//  AlternatingCapsHostTests.swift
+//  AlternatingCapsHostTests
+//
+//  Created by Faraz Alam on 5/5/25.
+//
+
+import Testing
+@testable import AlternatingCapsHost
+
+struct AlternatingCapsHostTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
